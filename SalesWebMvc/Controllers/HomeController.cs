b4c42ef8e9d598610742a46e1969e5447d473625
Fernando Calefi <fercalefi@gmail.com>
2020-02-25 08:51:19 -0300

    Primeiro controller em paginas Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Aplicativo de venda web Mvc em C#.";
+            ViewData["Email"] = "fercalefi@gmail.com";
 
             return View();
         }
